8ec9a8280035df4526c9a10eaf07f9791534c47c
Glaucio Dias <glaucio@noxxonsat.com.br>
2021-03-25 11:26:51 -0300

    .

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,15 @@ const { config } = require("dotenv");
 const { join } = require("path");
 const { ok } = require("assert");
 
-config();
+const env = process.env.NODE_ENV || "dev"
+ok(env === "prod" || env === "dev", "environment inválida! Ou prod ou dev")
+
+const configPath = join('./config', `.env.${env}`)
+
+config({
+    path: configPath
+})
+
 
 import express from "express";
 import cors from "cors";
