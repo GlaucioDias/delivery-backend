e5e77fda8005650b82665c6f1e80bc1f7a752361
GlaucioDias <fleevrl@gmail.com>
2021-03-25 22:36:25 -0300

    deploy

--- src/database/mongodb.ts
+++ src/database/mongodb.ts
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 class MongoDB {
     static connect() {
-        mongoose.connect(process.env.MONGO_URI, {
+        mongoose.connect(String(process.env.MONGO_URI), {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useFindAndModify: false
